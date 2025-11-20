91d501dfbf8b0e72dd038b7ff81f6ed703a3467a
Raja Ramesh Haddubangi <h.rajaramesh@gmail.com>
2025-11-20 13:15:37 +0530

    init

---
+++ app.py
@@ -0,0 +1,40 @@
+from google import genai
+import os
+import time
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_API_KEY") # If you are not using Colab you can set the API key directly
+
+# Create a client
+client = genai.Client(api_key=api_key)
+
+# Define the model you are going to use
+model_id =  "gemini-2.5-flash"
+
+
+
+# Define the prompt
+prompt = "get the table data from given pdf file. return the data in a table format. "
+file_path = "files/sample.pdf"
+
+_pdf = client.files.upload(file="files/sample.pdf", config={'display_name': 'pdf'})
+
+file_size = client.models.count_tokens(model=model_id,contents=_pdf)
+print(f'File: {_pdf.display_name} equals to {file_size.total_tokens} tokens')
+
+file = client.files.upload(file=file_path, config={'display_name': file_path.split('/')[-1].split('.')[0]})
+
+# Generate a response using the Person model
+start_time = time.time()
+response = client.models.generate_content(model=model_id, contents=[prompt, file], config={'response_mime_type': 'text/plain'})
+end_time = time.time()
+
+# Calculate and print the time taken
+elapsed_time = end_time - start_time
+print(f"\nTime taken to get response: {elapsed_time:.2f} seconds")
+
+# print the response
+print(response.text)
+
